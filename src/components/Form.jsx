bf940a29995849f50c8f72dc17ea786b2c3704f1
MRomyl <romyl.martushev@gmail.com>
2025-10-16 16:58:31 -0700

    d

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 import { useLocalStorage } from "../hooks/useLocalStorage";
+
 function Form() {
+  const keyName = "firstName";
+  const keyService = "serviceNumber";
+  
+  //lookup the init values or get defaults
+  const initName = useLocalStorage(keyName, "")
+  const initServNumber = useLocalStorage(keyService, 0);
+
+  //setup state
+  const [name, setName] = useState(initName)
+  const [serviceNumber, setServiceNumber] = useState(initServNumber)
+
+  useEffect(() => {
+    // console.log(name)
+    // console.log(serviceNumber)
+    localStorage.setItem(keyName, name)
+    localStorage.setItem(keyService, serviceNumber)
+  }, [name, serviceNumber]);
 
     return (
       <>
         <form style={{ display: "flex", flexDirection: "column" }}>
             <label htmlFor="name">Name:</label>
-            <input type="text" data-testid={"name"} />
+            <input type="text" data-testid={"name"} value={name} onChange={e => setName(e.target.value)} />
             <label htmlFor="service">Service Number:</label>
-            <input type="text" data-testid={"service"} />
+            <input type="text" data-testid={"service"} value={serviceNumber} onChange={e => setServiceNumber(e.target.value)} />
 
         </form>
         <h4>{name ? `Welcome, ${name}!` : "Enter your name"}</h4>
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
 
+
+
 export function useLocalStorage(key, initialValue = null) {
+    return localStorage.getItem(key) ?? initialValue;
 }
+
